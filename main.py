de269fdb516ecac5a4915c9ed1b0b117078e2545
Alina <alinarogova1991@gmail.com>
2024-09-30 20:59:17 +0300

    final update

--- main.py
+++ main.py
@@ -7,3 +7,6 @@ def farewell(name):
 if __name__ == "__main__":
     user_name = input("Enter a name: ")
     print(greet(user_name))
+    print(farewell(user_name))
+    print("text2")
+    print("Final")
