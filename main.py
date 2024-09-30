74425b37555e7a72eacec46f003d847161369e72
Alina <alinarogova1991@gmail.com>
2024-09-30 20:46:06 +0300

    new

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 def greet(name):
     return f"Hello, {name}"
+def farewell(name):
+    return f"Goodbye, {name}"
 if __name__ == "__main__":
     user_name = input("Enter a name: ")
     print(greet(user_name))
